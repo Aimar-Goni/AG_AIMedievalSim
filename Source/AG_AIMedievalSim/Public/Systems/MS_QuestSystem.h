543b81886945fb8cf593e3069ef0f67c28f0d3e9
Aimar-Goni <elgoni64@gmail.com>
2024-10-21 20:35:28 +0100

    Map changes

---
+++ Source/AG_AIMedievalSim/Public/Systems/MS_QuestSystem.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class AG_AIMEDIEVALSIM_API MS_QuestSystem
+{
+public:
+	MS_QuestSystem();
+	~MS_QuestSystem();
+};
